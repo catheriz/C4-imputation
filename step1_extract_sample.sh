8b6758857ab7548140641c67893872528032f95f
catheriz <53827903+catheriz@users.noreply.github.com>
2025-02-27 17:57:42 -0600

    Delete step1_extract_sample.sh

--- step1_extract_sample.sh
+++
@@ -1,7 +0,0 @@
-module load bcftools
-bcftools annotate -x ^INFO/DR2,^INFO/AF,^FORMAT/DS DMPM_C4_imputation_hg19.vcf.gz -Oz -o DMPM_C4_imputation_hg19_DS.vcf.gz
-
-(echo -e "ALT\tDR2\tAF\t$(bcftools query -l DMPM_C4_imputation_hg19_DS.vcf.gz | tr '\n' '\t' | sed 's/\t$//')"; \
-bcftools view -i 'ID=="C4"' DMPM_C4_imputation_hg19_DS.vcf.gz | bcftools query -f '%ALT\t%INFO/DR2\t%INFO/AF[\t%DS]\n') > dosages.txt
-
-
